a7af147d887453d9c2e052816a5621419e0dda74
lciamp <l.ciamp@yahoo.com>
2024-05-25 08:22:28 -0400

    add handlers and error check

--- main.go
+++ main.go
@@ -1,13 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
 // home handler function
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Snippet!"))
+	_, err := w.Write([]byte("Hello from Snippet!"))
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+// add snippetView handler function
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Display a specific snippet..."))
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
+
+// add a snippet handler function
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Display form for creating a new snippet..."))
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+
 }
 
 // main function
@@ -15,6 +36,9 @@ func main() {
 	// create a servemux and register the home function
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	// register new handlers
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting on: 4000")
 
