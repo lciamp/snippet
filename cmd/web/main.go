164261c75e65be09961a8a817d9a37f75e38914f
lciamp <l.ciamp@yahoo.com>
2024-05-26 13:14:58 -0400

    add cli flags for port at runtime

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// cli flags to be determined at run time
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// create servemux instance
 	mux := http.NewServeMux()
 	// create file server for static files
 	//fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -13,6 +19,7 @@ func main() {
 	// use mux.Handle() to register the files server as the handler for all URL paths that start with
 	// "/static/". For matching paths, strip "/static" prefix before the request reaches the file server
 	//mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
@@ -22,8 +29,8 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting on: localhost:4000")
+	log.Print("Starting on: localhost:%s", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
