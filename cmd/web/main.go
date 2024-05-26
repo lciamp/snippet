539af880b3fcc2fd353283f3bd369dd8f2a8f02b
lciamp <l.ciamp@yahoo.com>
2024-05-26 10:41:27 -0400

    custom file system to disable dir listings

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,12 +7,11 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	// create file server for static files
-	fileServer := http.FileServer(http.Dir("./ui/static"))
 
-	// use mux.Handle() to register the files server as the handler for all URL paths that start with
-	// "/static/". For matching paths, strip "/static" prefix before the request reaches the file server
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	// create file server for static files using custom file system from helpers.go
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", home) // restrict for / only
 	mux.HandleFunc("GET /snippet/view/{id}/{$}", snippetView)
