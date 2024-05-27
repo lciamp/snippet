139f50279372dad0665547a43894b578cf669ba5
lciamp <l.ciamp@yahoo.com>
2024-05-26 20:58:28 -0400

    add structured logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
 func main() {
 	// create custom cli flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	// add structured logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	// create mux
 	mux := http.NewServeMux()
@@ -18,18 +22,20 @@ func main() {
 	/*	fileServer := http.FileServer(http.Dir("./ui/static"))
 		mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))*/
 
-	//  create custom file server
+	//  create custom file server with dir listing disabled
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// add handler functions to endpoints
 	mux.HandleFunc("GET /{$}", home) // restrict for / only
 	mux.HandleFunc("GET /snippet/view/{id}/{$}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Printf("starting on: localhost%s", *addr)
-
+	// start server
+	logger.Info("starting on: localhost", "addr", *addr)
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
