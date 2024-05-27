62441909f26879edf1ac677a8a343748cd9481ad
lciamp <l.ciamp@yahoo.com>
2024-05-26 20:37:57 -0400

    add custom cli flag for port #

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// create custom cli flags
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	// create mux
 	mux := http.NewServeMux()
+
 	// create file server for static files
 	/*	fileServer := http.FileServer(http.Dir("./ui/static"))
 		mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))*/
@@ -21,8 +28,8 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting on: localhost:4000")
+	log.Printf("starting on: localhost%s", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
