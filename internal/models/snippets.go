bd7da5c60dfe4844afd6804c8aa7b59859828f53
lciamp <l.ciamp@yahoo.com>
2024-06-09 19:29:20 -0400

    implement insert method

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -21,7 +21,24 @@ type SnippetModel struct {
 
 // Insert function to insert into db
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	return 0, nil
+	// sql stmt to insert
+	stmt := `INSERT INTO snippets (title, content, created, expires) 
+	VALUES (?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	// exec the stmt to insert
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	// use LastInsertId to get the id of what we inserted
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	// LastInsertId returns a type int64, convert to normal int
+	return int(id), nil
 }
 
 // Get function to get a snippet from db
