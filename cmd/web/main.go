efcd913e264671edf0810fc39757f1c85c5de99e
lciamp <l.ciamp@yahoo.com>
2024-06-09 15:30:43 -0400

    set up db connection pool and verify

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql" // mysql driver
 )
 
 type application struct {
@@ -14,6 +17,8 @@ type application struct {
 func main() {
 	// create custom cli flags
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	// mysql data source name command line flag
+	dsn := flag.String("dsn", "web:hudson@/snippetbox?parseTime=true", "MySQL datasource name")
 	flag.Parse()
 
 	// add structured logger, use JSON logging, add debug log level and file/line number source
@@ -24,9 +29,32 @@ func main() {
 		logger: logger,
 	}
 
+	// db connection pool setup
+	db, err := openDB(*dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+
 	// start server
 	logger.Info("starting server", slog.String("addr", *addr))
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+// wrap soq.Open() and return sql.DB connection pool for DSN
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
