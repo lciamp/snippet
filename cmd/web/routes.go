62cbc00dea1c5727b3cd8f4a3ac1424e84c2663b
lciamp.eth <l.ciamp@yahoo.com>
2024-06-19 13:02:12 -0400

    wrap existing return chain with logRequest middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,7 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
-	// wrap mux in commonHeaders middleware
-	return commonHeaders(mux)
+	// 1. wrap mux in commonHeaders middleware
+	// 2. wrap existing chain with logRequest middleware
+	return app.logRequest(commonHeaders(mux))
 }
