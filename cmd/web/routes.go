6c21b247ee36c286ff41c0fb530bac52f70ea1fa
lciamp.eth <l.ciamp@yahoo.com>
2024-08-11 15:46:02 -0400

    update routes to use session manager

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,16 +10,20 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	//  create custom file server with dir listing disabled
+	// don't wrap static routes with session middleware
 	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static")})
-
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	// new middleware chain for session management / dynamic routes
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
+
 	// add handler functions to endpoints
-	mux.HandleFunc("GET /{$}", app.home) // restrict for / only
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	// switch back to handle instead of HandleFunc for dynamic middleware (ThenFunc returns a Handler)
+	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home)) // restrict for / only
+	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
+	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
 	// create middleware chain using our standard middleware
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
