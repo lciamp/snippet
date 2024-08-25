aa5d369c37a70cecf7cf153c9c793c4191105a0b
lciamp.eth <l.ciamp@yahoo.com>
2024-08-25 15:09:06 -0400

    add custom errors for login/signup

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -4,4 +4,13 @@ import (
 	"errors"
 )
 
-var ErrNoRecord = errors.New("models: no matching record found")
+var (
+	// ErrNoRecord no matching record
+	ErrNoRecord = errors.New("models: no matching record found")
+
+	// ErrInvalidCredentials login with incorrect email or password
+	ErrInvalidCredentials = err.New("models: invalid credentials")
+
+	// ErrDuplicateEmail signup with an email that's already in use
+	ErrDuplicateEmail = err.New("models: duplicate email")
+)
