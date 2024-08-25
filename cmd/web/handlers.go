0a89a3e52a09935c1e8d3a300dd691722c01a2b9
lciamp.eth <l.ciamp@yahoo.com>
2024-08-25 15:44:44 -0400

    update signup handler to use new template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -125,8 +125,17 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 }
 
 // user login handlers
+type userSignupForm struct {
+	Name                string `form:"name"`
+	Email               string `form:"email"`
+	Password            string `form:"password"`
+	validator.Validator `form:"-"`
+}
+
 func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Display signin form")
+	data := app.newTemplateData(r)
+	data.Form = userSignupForm{}
+	app.render(w, r, http.StatusOK, "signup.tmpl", data)
 }
 
 func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
