46ff63dcd562cf535db16570d1495da1ee85da8f
lciamp.eth <l.ciamp@yahoo.com>
2024-08-25 21:04:09 -0400

    add 2 validator functions and a regex pattern to comapre against emails

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,10 +1,15 @@
 package validator
 
 import (
+	"regexp"
 	"slices"
 	"strings"
 	"unicode/utf8"
 )
+
+// EmailRx use regex.MustCompile() function to check format of email addresses
+// returns a pointer to a 'compiled' regex type or panics
+var EmailRx = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 type Validator struct {
 	FieldErrors map[string]string
@@ -49,4 +54,14 @@ func MaxChars(value string, n int) bool {
 // PermittedValue return true if the value is in a list of permitted values
 func PermittedValue[T comparable](value T, permittedValues ...T) bool {
 	return slices.Contains(permittedValues, value)
+}
+
+// MinChars returns true if the value is less than n characters
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+// Matches returns true if a value matches a compiled regular expression
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
